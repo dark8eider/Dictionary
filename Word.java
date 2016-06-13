e57e3549e7a0d4ededa02842478d918aba428c71
dark8eider <dark8eider@mail.ru>
2016-06-13 11:29:27 +0300

    Created file Dictionary.txt and writed methods for class Word: addWord and seekWord

--- Word.java
+++ Word.java
@@ -1,10 +1,27 @@
 
-package Dictinary;
+package Dictionary;
 import java.util.Scanner;
 
 public class Word extends Dictionary{
     private Scanner in = new Scanner(System.in);
     private String word;
+    private Dictionary d = new Dictionary();
     
+    public Word(){
+        super();
+    }
+    
+    public void inputWord(){
+        System.out.println("Enter a word");
+        word = in.nextLine();
+    }
+    
+    public void sendAddWord(){
+        super.addWord(word);
+    }
+    
+    public void sendSeekWord(){
+        super.seekWord(word);
+    }
     
 }
